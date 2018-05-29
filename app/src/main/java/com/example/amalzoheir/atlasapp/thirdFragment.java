bd45ffb53b8f40c7f6406e9f5ffd5b0e13346cf0
Amalzoheir <A.Zamalzoheir95@gmail.com>
2018-05-29 05:58:47 +0200

    after adding adapter for all fragment

--- app/src/main/java/com/example/amalzoheir/atlasapp/thirdFragment.java
+++ app/src/main/java/com/example/amalzoheir/atlasapp/thirdFragment.java
@@ -6,12 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class thirdFragment extends Fragment {
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.third_section_fragment_layout,container,false);
+        View rootView=inflater.inflate(R.layout.fifth_section_fragment_layout,container,false);
+        ArrayList<TextAtlasInSections> textAtlasInSectionsArrayList=new ArrayList<TextAtlasInSections>();
+        textAtlasInSectionsArrayList.add(new TextAtlasInSections("first"));
+        textAtlasInSectionsArrayList.add(new TextAtlasInSections("second"));
+        textAtlasInSectionsArrayList.add(new TextAtlasInSections("third"));
+        TextInSectionAdapter itemAdapter=new TextInSectionAdapter(this.getActivity(),textAtlasInSectionsArrayList);
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+        listView.setAdapter(itemAdapter);
+        return rootView;
     }
 }
