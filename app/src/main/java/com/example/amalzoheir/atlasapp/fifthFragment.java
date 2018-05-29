fe8f3a3736d26d28b49f53bb79c4393b5ff1be6e
Amalzoheir <A.Zamalzoheir95@gmail.com>
2018-05-29 05:29:08 +0200

    after adding adapter

--- app/src/main/java/com/example/amalzoheir/atlasapp/fifthFragment.java
+++ app/src/main/java/com/example/amalzoheir/atlasapp/fifthFragment.java
@@ -3,15 +3,29 @@ package com.example.amalzoheir.atlasapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-public class fifthFragment extends Fragment {
+import java.util.ArrayList;
+
+public class fifthFragment extends Fragment{
+
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fifth_section_fragment_layout,container,false);
+       View rootView=inflater.inflate(R.layout.fifth_section_fragment_layout,container,false);
+       ArrayList<TextAtlasInSections> textAtlasInSectionsArrayList=new ArrayList<TextAtlasInSections>();
+       textAtlasInSectionsArrayList.add(new TextAtlasInSections("first"));
+        textAtlasInSectionsArrayList.add(new TextAtlasInSections("second"));
+        textAtlasInSectionsArrayList.add(new TextAtlasInSections("third"));
+        TextInSectionAdapter itemAdapter=new TextInSectionAdapter(this.getActivity(),textAtlasInSectionsArrayList);
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+        listView.setAdapter(itemAdapter);
+        return rootView;
     }
 }
